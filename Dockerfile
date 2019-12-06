752eaf7b370ab5f518eca51288c67356438e951b
James Rine <jlrine2@alaska.edu>
2019-12-06 00:40:23 -0900

    fixed errors

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,17 @@
 FROM python:slim
 
-ADD . /
+WORKDIR /app
+
+ADD app.py /app
+ADD data.json /app
+ADD scrape.py /app
+COPY templates /app/templates
+COPY static /app/static
+
 
 ENV MAX_VAL=2237
 
+COPY requirements.txt /app
 RUN pip install -r requirements.txt
 
-CMD ["python", "app.py"]
+CMD ["flask", "run", "--host", "0.0.0.0"]
--- app.py
+++ app.py
@@ -16,23 +16,23 @@ def getComic(num):
         return "<p>the 404th XKCD retruns a 404 error </p>"
     image = images[str(num)]
     
-    if num < 10 or os.environ["MAX_VAL"]-num < 10:
+    if num < 10 or int(os.environ['MAX_VAL'])-num < 10:
         return Markup(F"""<h1>{image['title']}</h1> <img src="../static/archive/{num}.jpg"/> <p>"{image['alt']}</p>""")
     return Markup(F"""<img class="lazy" data-src="../static/archive/{num}.jpg" title="{image['alt']}" alt="{image['title']}" />""")
+app.add_template_global(getComic, name="getComic")
 
 
 @app.route("/")
 def all_images():
-    max_val=os.environ["MAX_VAL"]
+    max_val=int(os.environ['MAX_VAL'])
     r = requests.get(F"https://xkcd.com/{max_val + 1}")
     if r.status_code == 200:
         scrape.download(max_val + 1)
-        os.environ["MAX_VAL"] = max_val + 1
+        os.environ['MAX_VAL'] = str(max_val + 1)
 
     
     return render_template("newFirst.html", images=images, maxVal=max_val)
 
 
 if __name__ == "__main__":
-    app.add_template_global(getComic, name="getComic")
     app.run()
